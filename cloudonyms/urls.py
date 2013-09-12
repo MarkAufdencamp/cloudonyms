00be357014f0a238756a82ceae52248a346f6a9d
Mark Aufdencamp <Mark@Aufdencamp.com>
2013-09-12 14:56:16 -0400

    Added admin and admindocs to urls.

--- cloudonyms/urls.py
+++ cloudonyms/urls.py
@@ -3,16 +3,20 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'cloudonyms.views.landing_page', name='home')
+    url(r'^$', 'cloudonyms.views.landing_page', name='home'),
     # Examples:
     # url(r'^$', 'cloudonyms.views.home', name='home'),
     # url(r'^cloudonyms/', include('cloudonyms.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
