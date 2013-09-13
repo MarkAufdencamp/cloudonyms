3b21da4aa998ace6aea12033d1217a86e6b60627
Mark Aufdencamp <Mark@Aufdencamp.com>
2013-09-13 17:16:14 -0400

    Converted landing_page view to an inherited template.

--- cloudonyms/views.py
+++ cloudonyms/views.py
@@ -1,9 +1,4 @@
-from django.http import HttpResponse
-from django.template import Template, Context
+from django.shortcuts import render_to_response
 
 def landing_page(request):
-    fp = open('./cloudonyms/views/landing_page_template.html')
-    t = Template(fp.read())
-    fp.close()
-    html = t.render(Context())
-    return HttpResponse(html)
+    return render_to_response('landing_page_template.html')
